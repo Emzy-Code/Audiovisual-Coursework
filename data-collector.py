813dda85e20b7414a9092987ded8b54743ca1421
Emzy-Code <emilijazic@gmail.com>
2024-10-23 10:38:34 +0100

    Created data collector
    used Ryan's help for semantics on how to communicate the file naming format in comments

---
+++ data-collector.py
@@ -0,0 +1,14 @@
+# -Created 23 Oct, Emilija
+# This file is used to record data and automatically place it within data folder
+# each file should have the format of name(0-9)(0-9).wav (example: ryan00.wav)
+# sound device used - :
+# voice actor - :
+
+import sounddevice as sd
+import soundfile as sf
+
+fs = 16000
+seconds = 5
+r = sd.rec(seconds * fs, samplerate=fs, channels=1)
+sd.wait()
+sf.write('./data/muneeb01.wav', r, fs) #change wav file name using naming convension above
