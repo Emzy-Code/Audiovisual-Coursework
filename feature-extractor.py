a2a0657919fbbf3bf24d90b6764ccac7d026ee2c
Emzy-Code <emilijazic@gmail.com>
2024-10-30 14:29:47 +0000

    Added frame stuff

--- feature-extractor.py
+++ feature-extractor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import soundfile as sf
 
 def magAndPhase(speechFrame):
     hamming_window = np.hamming(len(speechFrame))
@@ -15,3 +16,27 @@ def linearRectangularFilterbank(magspec, fbankNum):
     for i in range(len(fbank)):
         fbank[i] = sum(magspec[i * 32:i + 32])
     return fbank
+
+def file_feature_extraction(soundfile):
+    x, samplerate = sf.read(soundfile)
+    length = len(x)
+    print(length)
+    numFrames = 250
+    frameLength = int(len(x) /numFrames)
+
+    print(frameLength)
+    all_magnitude_spectra = []
+    all_phase_spectra = []
+    for frame in range(0, numFrames):
+        firstSample = frame * frameLength  # floor(frameLength/2) <- replace with frameLength to make frames overlap
+        lastSample = firstSample + frameLength
+        shortTimeFrame = x[firstSample:lastSample]
+        magSpec, phaseSpec = magAndPhase(shortTimeFrame)
+        all_magnitude_spectra.append(magSpec)
+        all_phase_spectra.append(phaseSpec)
+    return all_magnitude_spectra, all_phase_spectra
+
+
+
+
+mag, phase = file_feature_extraction('./data/a.wav')
