eb0629865932754a40dd295f075d32deab2acbbb
Emzy-Code <emilijazic@gmail.com>
2024-11-03 11:57:23 +0000

    Update acoustic-modelling.py

--- acoustic-modelling.py
+++ acoustic-modelling.py
@@ -10,3 +10,43 @@ from tf_keras.layers import Dense, Activation, Flatten, Conv2D, InputLayer, MaxP
 from tf_keras.optimizers import Adam
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+
+
+def create_model():
+    numClasses = 10
+    model = Sequential()
+    model.add(InputLayer(input_shape=(40, 21, 1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(Flatten())
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dense(numClasses))
+    model.add(Activation('softmax'))
+    return model
+
+
+data = []
+labels = []
+max_frames = 21
+for mfcc_file in sorted(glob.glob('mfccs/*.npy')):
+    mfcc_data = np.load(mfcc_file)
+    mfcc_data = np.pad(mfcc_data, ((0, 0), (0, max_frames - mfcc_data.shape[1])))
+    # if mfcc_data.shape[1] > max_frames:
+    #    max_frames = mfcc_data.shape[1]
+    data.append(mfcc_data)
+
+    stemFilename = (Path(os.path.basename(mfcc_file))).stem
+    label = stemFilename.split('_')
+    labels.append(label[0])
+labels = np.array(labels)
+data = np.array(data)
+data = data / np.max(data)
+
+LE = LabelEncoder()
+classes = ['muneeb', '', '2', '3', '4', '5', '6', '7', '8', '9']
+LE = LE.fit(classes)
+labels = to_categorical(LE.transform(labels))
+X_train, X_tmp, y_train, y_tmp = train_test_split(data, labels, test_size=0.2, random_state=0)
+X_val, X_test, y_val, y_test = train_test_split(X_tmp, y_tmp, test_size=0.5, random_state=0)
+
