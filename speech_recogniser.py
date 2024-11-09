05d6be4b945f40c25a860c1b065f301b1fd56f57
WookWook04 <ryanelvin3@gmail.com>
2024-11-09 23:39:09 +0000

    Added validation
    Program no longer breaks if no audio files are present for testing :+1:

--- speech_recogniser.py
+++ speech_recogniser.py
@@ -55,60 +55,61 @@ for mfcc_file in sorted(glob.glob('test_data/mfccs/*.npy')):
     label = stemFilename.split('_')
     labels.append(label[0])
 
+if len(labels)>0:
+    positions = []
+    finalPositions = []
 
-positions = []
-finalPositions = []
+    def get_number(order):
+        num = order.split('_')[1]
+        print("num: ", num)
+        return int(num)
 
-def get_number(order):
-    num = order.split('_')[1]
-    print("num: ", num)
-    return int(num)
+    print("order: ", orderedList)
+    orderedList = (sorted(orderedList, key = lambda order: get_number(order)))
+    print("sorted: ", orderedList)
+    for i in range(len(orderedList)):
+        positions.append(i)
 
-print("order: ", orderedList)
-orderedList = (sorted(orderedList, key = lambda order: get_number(order)))
-print("sorted: ", orderedList)
-for i in range(len(orderedList)):
-    positions.append(i)
+    print("labels: ", labels)
+    print("Final Pos: " ,finalPositions)
+    LE = LabelEncoder()
+    LE = LE.fit(classes)
+    labels = to_categorical(LE.transform(labels))
+    data = np.array(data)
+    data = data / np.max(data)
 
-print("labels: ", labels)
-print("Final Pos: " ,finalPositions)
-LE = LabelEncoder()
-LE = LE.fit(classes)
-labels = to_categorical(LE.transform(labels))
-data = np.array(data)
-data = data / np.max(data)
-
-X,y = data,labels
-model = load_model("my_model.keras")
-predicted_probs = model.predict(X, verbose=0)
-predicted = np.argmax(predicted_probs, axis=1)
-print("Predicted: ", predicted)
-actual = np.argmax(y, axis=1)
-#print(np.unique(predicted))
-accuracy = metrics.accuracy_score(actual, predicted)
-print(f'Accuracy: {accuracy * 100}%')
-#print(X[0, :, :])
-predicted_prob = model.predict(np.expand_dims(X[0, :, :],
-                                              axis=0), verbose=0)
-predicted_id = np.argmax(predicted_prob, axis=1)
-
-
-predicted_classes = []
-
-for i in range(len(predicted)):
-    predicted_classes.append(classes[predicted[i]])
-
-result=[]
-for i in range(len(unorderedList)):
-    result.append(0)
-    finalPositions.append(orderedList.index(unorderedList[i]))
-
-print(positions)
-print(finalPositions)
+    X,y = data,labels
+    model = load_model("my_model.keras")
+    predicted_probs = model.predict(X, verbose=0)
+    predicted = np.argmax(predicted_probs, axis=1)
+    print("Predicted: ", predicted)
+    actual = np.argmax(y, axis=1)
+    #print(np.unique(predicted))
+    accuracy = metrics.accuracy_score(actual, predicted)
+    print(f'Accuracy: {accuracy * 100}%')
+    #print(X[0, :, :])
+    predicted_prob = model.predict(np.expand_dims(X[0, :, :],
+                                                  axis=0), verbose=0)
+    predicted_id = np.argmax(predicted_prob, axis=1)
 
 
-for i in range(len(predicted_classes)):
-    result[finalPositions[i]] = predicted_classes[i]
+    predicted_classes = []
 
-print(result)
+    for i in range(len(predicted)):
+        predicted_classes.append(classes[predicted[i]])
 
+    result=[]
+    for i in range(len(unorderedList)):
+        result.append(0)
+        finalPositions.append(orderedList.index(unorderedList[i]))
+
+    print(positions)
+    print(finalPositions)
+
+
+    for i in range(len(predicted_classes)):
+        result[finalPositions[i]] = predicted_classes[i]
+
+    print(result)
+else:
+    print("No audio files. Please re-run.")
