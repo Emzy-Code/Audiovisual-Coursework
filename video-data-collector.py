83af60e544cf2f9c7e3d09ff21bc8fec85c4ac40
Emzy-Code <emilijazic@gmail.com>
2024-12-06 23:30:09 +0000

    Update video-data-collector.py

--- video-data-collector.py
+++ video-data-collector.py
@@ -13,42 +13,44 @@ import os
 load_dotenv()
 password = os.getenv('PASSWORD')
 print(password)
-ws = obsws("localhost", 4455)
+ws = obsws("localhost", 4444)
 ws.connect()
 load_dotenv()
 record_count = 0
 
 classes = sorted(['Muneeb',
-           'Zachary',
-           'Sebastian',
-           'Danny',
-           'Louis',
-           'Ben',
-           'Seb',
-           'Ryan',
-           'Krish',
-           'Christopher',
-           'Kaleb',
-           'Konark',
-           'Amelia',
-           'Emilija',
-           'Naima',
-           'Leo',
-           'Noah',
-           'Josh',
-           'Joey',
-           'Kacper'])
+                  'Zachary',
+                  'Sebastian',
+                  'Danny',
+                  'Louis',
+                  'Ben',
+                  'Seb',
+                  'Ryan',
+                  'Krish',
+                  'Christopher',
+                  'Kaleb',
+                  'Konark',
+                  'Amelia',
+                  'Emilija',
+                  'Naima',
+                  'Leo',
+                  'Noah',
+                  'Josh',
+                  'Joey',
+                  'Kacper'])
 nameCount = 0
 try:
     while True:
         record_count += 1
-        nameCount = nameCount + (1/20)
+        nameCount = nameCount + (1 / 20)
         print(f"Video {record_count} \n Name: {classes[math.floor(nameCount)]}")
-        ws.call(requests.SetFilenameFormatting("TESTNAME" + str(record_count)))
-        ws.call(requests.StartRecording())
+        string = f"TESTNAME {record_count}"
+
+        response = ws.call(requests.StartRecording())
         time.sleep(5)  # Record for 5 seconds
         # Stop recording
         ws.call(requests.StopRecording())
+        ws.call(requests.SetFilenameFormatting(**{f'filename-formatting': string}))
         time.sleep(1)  # Short delay before starting again
 except KeyboardInterrupt:
     pass
